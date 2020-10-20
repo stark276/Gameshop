ef1bf13b73b08e840bd21bb0988c3c8ae7ad5b16
Kaniet Oskonbaev <lmuzz1993@gmail.com>
2020-10-20 11:49:05 -0700

    [Add] Transaction model

--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('login', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('signup', views.signup, name='signup'),
+    path('home', views.home, name='home'),
 
 ]
